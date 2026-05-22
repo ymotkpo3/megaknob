1dec2d3a36e20cc862e65d31b0587087c39d46cb
Tom D.H. <tomasdelgadoh@gmail.com>
2026-05-22 15:08:24 -0300

    3 button functionality
    Now I can control with 3 buttons different things through serial messages.

--- ArduinoUNO/ArduinoUNO.ino
+++ ArduinoUNO/ArduinoUNO.ino
@@ -1,20 +1,44 @@
-bool pressed = false;
+bool selpressed = false;
+bool dwnpressed = false;
+bool uppressed = false;
 
 void setup() {
   Serial.begin(9600);
 
+  pinMode(2, INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
 }
 
 void loop() {
 
-  if (digitalRead(4) == HIGH) {
-    pressed = true;
+    if (digitalRead(2) == HIGH) {
+    dwnpressed = true;
   }
 
-  if (digitalRead(4) == LOW && pressed == true) {
+  if (digitalRead(2) == LOW && dwnpressed == true) {
+    Serial.println("voldwn");
+    dwnpressed = false;
+    delay(500);
+  }
+
+  if (digitalRead(4) == HIGH) {
+    selpressed = true;
+  }
+
+  if (digitalRead(4) == LOW && selpressed == true) {
     Serial.println("select");
-    pressed = false;
+    selpressed = false;
+    delay(500);
+  }
+
+  if (digitalRead(7) == HIGH) {
+    uppressed = true;
+  }
+
+  if (digitalRead(7) == LOW && uppressed == true) {
+    Serial.println("volup");
+    uppressed = false;
     delay(500);
   }
 
